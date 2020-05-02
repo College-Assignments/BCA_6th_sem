9eaf3f89e4a9af365e9f00b56a07e6580f93b2ac
Suraj Mandal <surajmandalcell@gmail.com>
2020-05-02 07:37:15 +0530

    Solution7

--- Java Practical Assignment/Solution7.java
+++ Java Practical Assignment/Solution7.java
@@ -1,3 +1,13 @@
-public class Solution7 {
+import java.util.Arrays;
 
+public class Solution7 {
+  public static void main(String[] args) {
+    Integer arr[] = { 10, 123, 1235, 636, 6234, 43, 234, 66, 343 };
+    Integer sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+    }
+    System.out.println("\n\nInitial Array: " + Arrays.toString(arr));
+    System.out.println("\nSum of all numbers in array is: " + sum);
+  }
 }
