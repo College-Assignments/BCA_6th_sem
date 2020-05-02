8b55cfd5acf1e53c4e134ed9c5320ff59ff9d1f4
Suraj Mandal <surajmandalcell@gmail.com>
2020-05-02 21:04:13 +0530

    Solution3 and 4

--- Java Practical Assignment/Solution3.java
+++ Java Practical Assignment/Solution3.java
@@ -1,3 +1,61 @@
-public class Solution3 {
+import java.time.LocalTime;
+import java.util.Scanner;
 
+import utility.utilities;
+
+class Time {
+  int hours;
+  int minutes;
+
+  void readTime() {
+    hours = LocalTime.now().getHour();
+    minutes = LocalTime.now().getMinute();
+  }
+
+  void displayTime() {
+    System.out.println("Current time: " + hours + "H " + minutes + "m");
+  }
+
+  void sumOfTimes(int h1, int h2, int m1, int m2, int s1, int s2) {
+    int totalHours = h1 + h2;
+    int totalMinutes = m1 + m2;
+    int totalSeconds = s1 + s2;
+    if (totalSeconds >= 60) {
+      totalMinutes++;
+      totalSeconds = totalSeconds % 60;
+    }
+    if (totalMinutes >= 60) {
+      totalHours++;
+      totalMinutes = totalMinutes % 60;
+    }
+    System.out.println("\n\n Sum of times is: " + totalHours + "H " + totalMinutes + "m " + totalSeconds + "s");
+  }
+}
+
+public class Solution3 {
+  public static void main(String[] args) {
+    utilities.cls();
+
+    Time obj = new Time();
+    obj.readTime();
+    obj.displayTime();
+    Scanner in = new Scanner(System.in);
+    System.out.println("\nSum of two 'time'");
+    System.out.println("Enter time 1 (H/m/s): ");
+    System.out.println("\n\tEnter Hour: ");
+    int h1 = in.nextInt();
+    System.out.println("\tEnter Minute: ");
+    int m1 = in.nextInt();
+    System.out.println("\tEnter second: ");
+    int s1 = in.nextInt();
+    System.out.println("\nEnter time 2 (H/m/s): ");
+    System.out.println("\n\tEnter Hour: ");
+    int h2 = in.nextInt();
+    System.out.println("\tEnter Minute: ");
+    int m2 = in.nextInt();
+    System.out.println("\tEnter second: ");
+    int s2 = in.nextInt();
+    obj.sumOfTimes(h1, h2, m1, m2, s1, s2);
+    in.close();
+  }
 }
--- Java Practical Assignment/Solution4.java
+++ Java Practical Assignment/Solution4.java
@@ -1,3 +1,90 @@
-public class Solution4 {
+import java.util.Scanner;
+import java.util.Vector;
 
+import utility.utilities;
+
+class Viktor {
+  Vector<Integer> vic;
+
+  // A. Craete vector
+  void createVector() {
+    vic = new Vector<Integer>();
+    vic.add(7);
+    vic.add(6);
+    vic.add(9);
+    vic.add(2);
+    vic.add(3);
+    System.out.println("Vectors created");
+  }
+
+  // B. Modify element
+  void modifyElement() {
+    System.out.println("Before modifications the vector contains: " + vic);
+    vic.set(1, 12);
+    System.out.println("Modified Vector elements are: " + vic);
+  }
+
+  // C. Multiply by a scalar value
+  double mul(Vector a, int b) {
+    return mul(a) + b;
+  }
+
+  static double mul(Vector value) {
+    // returns sum of all elements in vector,
+    // but since there is only one element in vector it will return the first value
+    return value.stream().mapToDouble(x -> (double) x).sum();
+  }
+
+  // D. Display Vectors
+  void display() {
+    System.out.println("Vector value is: " + vic);
+  }
+
+  void display(Vector prop) {
+    System.out.println("Current vector is: " + prop);
+  }
+
+  // E. Add two vectors
+  double sum(Vector value, Vector value1) {
+    return sum(value) + sum(value1);
+  }
+
+  static double sum(Vector value) {
+    return value.stream().mapToDouble(x -> (double) x).sum();
+  }
+
+}
+
+public class Solution4 {
+  public static void main(String[] args) {
+    utilities.cls();
+    // Declare
+    Viktor obj = new Viktor();
+    Vector a = new Vector();
+    Vector b = new Vector();
+    Vector c = new Vector();
+
+    Scanner in = new Scanner(System.in);
+
+    // Work
+    System.out.println("Enter value in first vector");
+    double aI = in.nextInt();
+    System.out.println("\nEnter value in second vector\n");
+    double bI = in.nextInt();
+    a.add(aI);
+    b.add(bI);
+    System.out.println("Addition of two vectors is: " + obj.sum(a, b) + "\n\n");
+
+    System.out.println("Vector multiplication with scalar\nEnter vector part: ");
+    double cI = in.nextDouble();
+    c.add(cI);
+    System.out.println("\nEnter scalar part: ");
+    int d = in.nextInt();
+    System.out.println("Product of vector and scalar is: " + obj.mul(c, d) + "\n");
+
+    System.out.println("\nOperations on predefined vectors:\n");
+    obj.createVector();
+    obj.modifyElement();
+    in.close();
+  }
 }
