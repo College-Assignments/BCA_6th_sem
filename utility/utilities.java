8b1f968b2857dc6f8c496573ff3ba97bd4f8a237
Suraj Mandal <surajmandalcell@gmail.com>
2020-05-02 07:16:32 +0530

    eh

--- utility/utilities.java
+++
@@ -1,8 +0,0 @@
-package utility;
-
-public class utilities {
-    public final static void cls() {
-        System.out.println("Clearing...");
-        System.out.println("\n\n\n\n\n\n\n\n\n");
-    }
-}
